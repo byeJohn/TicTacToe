83936507050a69b655e0130be8e7621952d3200a
John Nguyen <75657889+byeJohn@users.noreply.github.com>
2022-11-07 15:32:09 -0500

    Update TicTacToe.java

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,6 +1,9 @@
 import java.util.*;
 public class TicTacToe {
 
+   static ArrayList<Integer> playerPositions = new ArrayList<Integer>();
+   static ArrayList<Integer> cpuPositions = new ArrayList<Integer>();
+   
    public static void main(String[] args) {
    
       char [] [] gameBoard = {{' ', '|', ' ', '|', ' '},
@@ -12,9 +15,10 @@ public class TicTacToe {
       printGameBoard(gameBoard);
       
       // prompts the user for input.
-      Scanner scan = new Scanner(System.in);
       
       while(true) {
+         Scanner scan = new Scanner(System.in);
+         
          System.out.println("Enter your placement (1-9): ");
          int playerPos = scan.nextInt();
             
@@ -83,6 +87,24 @@ public class TicTacToe {
             break;
       }
       
+   }
+   
+   
+   public static String checkWinner() {
+      
+      List topRow = Arrays.asList(1,2,3);
+      List midRow = Arrays.asList(4,5,6);
+      List botRow = Arrays.asList(7,8,9);
+      
+      List leftCol = Arrays.asList(1,4,7);
+      List midCol = Arrays.asList(2,5,8);
+      List rightCol = Arrays.asList(3,6,9);
+      
+      List cross1 = Arrays.asList(1,5,9);
+      List cross2 = Arrays.asList(7,5,3);
+      
+      
+      return "";  
    }
    
    
