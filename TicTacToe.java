370ffd23b97ab53f2e131a7876309d110f011b1d
John Nguyen <75657889+byeJohn@users.noreply.github.com>
2022-11-06 23:42:00 -0500

    Update TicTacToe.java

--- TicTacToe.java
+++ TicTacToe.java
@@ -18,37 +18,9 @@ public class TicTacToe {
       
       System.out.println(pos);
       
-      switch(pos) {
-         case 1:
-            gameBoard[0][0] = 'X';
-            break;
-         case 2:
-            gameBoard[0][02] = 'X';
-            break;
-         case 3:
-            gameBoard[0][0] = 'X';
-            break;
-         case 4:
-            gameBoard[0][0] = 'X';
-            break;
-         case 5:
-            gameBoard[0][0] = 'X';
-            break;
-         case 6:
-            gameBoard[0][0] = 'X';
-            break;
-         case 7:
-            gameBoard[0][0] = 'X';
-            break;
-         case 8:
-            gameBoard[0][0] = 'X';
-            break;
-         case 9:
-            gameBoard[0][0] = 'X';
-            break;
+      
+      printGameBoard(gameBoard);
 
-
-      }
    }
    
    // Prints out gameBoard  
@@ -60,5 +32,50 @@ public class TicTacToe {
          System.out.println();
       }
    }
+   
+   public static void placePiece(char[][] gameBoard, int pos, String user) {
+   
+         char
+   
+         if(user.equals("player")) {
+            symbol = 'X';
+         }
+   
+         switch(pos) {
+         case 1:
+            gameBoard[0][0] = 'X';
+            break;
+         case 2:
+            gameBoard[0][2] = 'X';
+            break;
+         case 3:
+            gameBoard[0][4] = 'X';
+            break;
+         case 4:
+            gameBoard[2][0] = 'X';
+            break;
+         case 5:
+            gameBoard[2][2] = 'X';
+            break;
+         case 6:
+            gameBoard[2][4] = 'X';
+            break;
+         case 7:
+            gameBoard[4][0] = 'X';
+            break;
+         case 8:
+            gameBoard[4][2] = 'X';
+            break;
+         case 9:
+            gameBoard[4][4] = 'X';
+            break;
+      }
+      
+   }
+   
+   
+   
+   
+   
    
 } // End of TicTacToe class.
